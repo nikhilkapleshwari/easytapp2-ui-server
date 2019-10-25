2335812bca9c6146410d50ac850c883237d879c1
nikhil kapleshwari <nikhil1632@gmail.com>
2019-10-25 23:41:56 +0530

    Change path of index.html

--- easytapp2_server.js
+++ easytapp2_server.js
@@ -3,14 +3,14 @@ const bodyParser = require('body-parser');
 var cors = require('cors');
 
 var app = express();
-var _dirname='/root/apps/projects/node_projects/EasyTapp';
+var _dirname='/apps/easytapp2/easytapp2_server';
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use(express.static(_dirname+'/dist/MyApp'));
+app.use(express.static(_dirname+'/dist/EasyTappUI'));
 
 app.all('/',(req,res)=>{
-res.status(200).sendFile(_dirname+'/dist/MyApp/index.html');
+res.status(200).sendFile(_dirname+'/dist/EasyTappUI/index.html');
 });
 
 app.listen(3009);
